42285133c9809ce50aa86a04c97921f2f7fbdcee
Matthias Kurz <m.kurz@irregular.at>
2019-07-28 11:30:19 +0200

    Update code according to comments in gist

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -4,28 +4,25 @@ import akka.stream.javadsl.Source;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
-import scala.Function0;
-import scala.Tuple2;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeController extends Controller {
 
     public Result index() {
-        final List<Tuple2<String, Function0<InputStream>>> filesToZip = new ArrayList<>();
+        final List<ZipToStreamFlow.ZipSource> filesToZip = new ArrayList<>();
 
-        filesToZip.add(Tuple2.apply("myfile1.txt", fileAsLazyInputStream("/tmp/file1.txt")));
-        filesToZip.add(Tuple2.apply("myfile2.txt", fileAsLazyInputStream("/tmp/file2.txt")));
-        filesToZip.add(Tuple2.apply("subfolder/myfile3.txt", fileAsLazyInputStream("/tmp/file3.txt")));
+        filesToZip.add(new ZipToStreamFlow.ZipSource("myfile1.txt", fileAsLazyInputStream("/tmp/file1.txt")));
+        filesToZip.add(new ZipToStreamFlow.ZipSource("myfile2.txt", fileAsLazyInputStream("/tmp/file2.txt")));
+        filesToZip.add(new ZipToStreamFlow.ZipSource("subfolder/myfile3.txt", fileAsLazyInputStream("/tmp/file3.txt")));
         // For s3 you have to do something like this:
-        //filesToZip.add(Tuple2.apply("file_from_s3.txt", (scala.Function0) () -> s3.getInputStream(storagePath)));
+        //filesToZip.add(new ZipToStreamFlow.ZipSource("file_from_s3.txt", (scala.Function0) () -> s3.getInputStream(storagePath)));
 
         return ok()
-                .chunked(Source.from(filesToZip).via(StreamedZip$.MODULE$.apply())) // or: new StreamedZip(64 * 1024) to set bufferSize
+                .chunked(Source.from(filesToZip).via(new ZipToStreamFlow(64 * 1024))) // also try e.g. 8192 as bufferSize
                 .as("application/zip")
                 .withHeaders(Http.HeaderNames.CONTENT_DISPOSITION, "attachment; filename=backup.zip");
     }
